372e78fb458039b29687b1411cc1f30148ae37f1
Richmond <rbaafi@cnm.edu>
2020-01-23 15:00:59 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/model/Card.java
@@ -0,0 +1,25 @@
+package edu.cnm.deepdive.model;
+
+public class Card {
+
+  private Suit suit;
+  private Rank rank;
+
+  public Card(Suit suit, Rank rank) {
+    this.suit = suit;
+    this.rank = rank;
+  }
+
+  public Suit getSuit() {
+    return suit;
+  }
+
+  public Rank getRank() {
+    return rank;
+  }
+
+  @Override
+  public String toString() {
+    return rank.symbol() + suit.symbol();
+  }
+}
