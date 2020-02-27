7411c42de9fcde2f5d6a761e9b4823c575847841
Richmond <rbaafi@cnm.edu>
2020-02-27 10:42:36 -0700

    Card implements comparable.

--- src/edu/cnm/deepdive/model/Card.java
+++ src/edu/cnm/deepdive/model/Card.java
@@ -1,23 +1,40 @@
+/*
+ *  Copyright 2019 Deep Dive Coding/CNM Ingenuity
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package edu.cnm.deepdive.model;
 
+import java.util.Comparator;
 import java.util.Objects;
 
 /**
- * Encapsulate a single playing card as a combination of {@Link Suit} and {@Link Rank}. Instance
- * of this class are immutable
- * @author ro417
+ * Encapsulates a single playing card as a combination of {@link Suit} and {@link Rank}. Instances
+ * of this class are immutable.
+ *
+ * @author Nicholas Bennett &amp; Deep Dive Coding Java + Android Cohort 9.
  */
-
-public class Card {
+public class Card implements Comparable<Card> {
 
   private final Suit suit;
   private final Rank rank;
   private final int hash;
 
   /**
-   * Initializes the Card instances with the specified {@Link Suit} and {@Link Rank}.
-   * @param suit{@Link Suit} value of card.
-   * @param rank{@Rank Rank} value of card.
+   * Initializes this {@code Card} instance with the specified {@link Suit} and {@link Rank}.
+   *
+   * @param suit {@link Suit} value of card.
+   * @param rank {@link Rank} value of card.
    */
   public Card(Suit suit, Rank rank) {
     this.suit = suit;
@@ -26,37 +43,29 @@ public class Card {
   }
 
   /**
-   * Return {@link Suit} of this Card intance.
-   * @return
+   * Returns {@link Suit} of this {@code Card} instance.
    */
-
   public Suit getSuit() {
     return suit;
   }
 
   /**
-   * Returns {@Link Rank} of this Card instance.
+   * Returns {@link Rank} of this {@code Card} instance.
    */
-
   public Rank getRank() {
     return rank;
   }
+
   /**
-   * Returns {@Link Suit} of this Card instance.
+   * Concatenates and returns the values returned by {@link #getRank()}{@link Rank#symbol()
+   * symbol()} and {@link #getSuit()}{@link Suit#symbol() symbol()}.
+   *
+   * @return concatenated {@link Rank} &amp; {@link Suit} symbols.
    */
-
-
   @Override
   public String toString() {
     return rank.symbol() + suit.symbol();
   }
-
-  /**
-   * Returns{@Link Color} instance value for this suit.
-   *
-   * @return {@Link Color} value.
-   */
-
 
   @Override
   public int hashCode() {
@@ -68,14 +77,24 @@ public class Card {
     boolean comparison = false;
     if (obj == this) {
       comparison = true;
-    } else if (obj instanceof Card && obj.hashCode() ==hash) {
+    } else if (obj instanceof Card) {
       Card other = (Card) obj;
-      if (suit == other.suit && rank == other.rank) {
+      if (hash == other.hash && suit == other.suit && rank == other.rank) {
         comparison = true;
       }
     }
     return comparison;
   }
 
+  @Override
+  public int compareTo(Card other) {
+    return Comparator.comparing(Card::getSuit).thenComparing(Card::getRank).compare(this, other);
+//    int result = getSuit().compareTo(other.getSuit());
+//    if (result == 0) {
+//      result = getRank().compareTo(other.getRank());
+//    }
+//    return result;
+//  }
 
+  }
 }
--- src/edu/cnm/deepdive/model/Deck.java
+++ src/edu/cnm/deepdive/model/Deck.java
@@ -1,34 +1,42 @@
+/*
+ *  Copyright 2019 Deep Dive Coding/CNM Ingenuity
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package edu.cnm.deepdive.model;
 
-/**
- * Encapsulate a single playing card as a combination of {@Link Suit} and {@Link Rank}. Instance
- * of this class contains values.
- * @author ro417
- */
-
-
-import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-public class Deck implements Comparator<Card> {
+/**
+ * Encapsulates a single deck of standard playing cards, which can be shuffled, dealt one card at a
+ * time, and reshuffled (automatically gathering all previously dealt cards back into the deck).
+ *
+ * @author Nicholas Bennett &amp; Deep Dive Coding Java + Android Cohort 9.
+ */
+public class Deck {
 
   private List<Card> cards;
   private List<Card> dealt;
 
   /**
-   * suit{@Link Suit} value of card.
-   * Rank{@Rank Rank} value of card.
+   * Initializes this instance with 52 cards, sorted by suit and rank.
    */
-
-
-  public Deck(){
+  public Deck() {
     cards = new ArrayList<>();
     dealt = new LinkedList<>();
     for (Suit s : Suit.values()) {
@@ -39,10 +47,11 @@ public class Deck implements Comparator<Card> {
   }
 
   /**
-   * Suit{@Link Suit} value of cards being sorted out.
+   * Removes and returns a single {@link Card} from the deck. If there are no more cards to be
+   * dealt, a {@code null} value is returned.
+   *
+   * @return top {@link Card} instance on deck.
    */
-
-
   public Card deal() {
     Card card = cards.isEmpty() ? null : cards.remove(0);
     if (card != null) {
@@ -51,6 +60,12 @@ public class Deck implements Comparator<Card> {
     return card;
   }
 
+  /**
+   * Shuffles the deck contents (after returning any previously dealt cards to the deck) using the
+   * specified source of randomness.
+   *
+   * @param rng {@link Random} instance, used as source of randomness for shuffle.
+   */
   public void shuffle(Random rng) {
     gather();
     Collections.shuffle(cards, rng);
@@ -61,39 +76,31 @@ public class Deck implements Comparator<Card> {
     dealt.clear();
   }
 
-
+  /**
+   * Returns the number of {@link Card} instances remaining to be dealt.
+   */
   public int remaining() {
     return cards.size();
   }
 
+  /**
+   * Returns the number of {@link Card} instances that have been dealt since the last shuffle (or
+   * since instance initialization).
+   */
   public int dealt() {
     return dealt.size();
   }
-  /**Rank{@Link Rank} value of cards being sorted out.
-   * */
-
 
   @Override
   public String toString() {
     return cards.toString();
   }
 
-  public static void main(String[] args) {
-    Deck deck = new Deck();
-    System.out.println(deck);
-    deck.shuffle(new SecureRandom());
-    System.out.println(deck);
-  }
-
   public void sort(boolean gather) {
     if (gather) {
       gather();
     }
-    cards.sort(this);
+    cards.sort(null);
   }
 
-  @Override
-  public int compare(Card card1, Card card2) {
-    return Comparator.comparing(Card::getSuit).thenComparing(Card::getRank).compare(card1, card2);
-  }
 }
