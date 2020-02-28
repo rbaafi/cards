6cf35d11deccc197968cff0a6bc992718dadfd69
Richmond <rbaafi@cnm.edu>
2020-02-28 08:05:18 -0700

    Created new class, Shoe.

--- src/edu/cnm/deepdive/model/Shoe.java
+++ src/edu/cnm/deepdive/model/Shoe.java
@@ -1,6 +1,9 @@
 package edu.cnm.deepdive.model;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
@@ -9,5 +12,31 @@ public class Shoe {
   private List<Card> cards;
   private List<Card> dealt;
   private Random rng = new Random();
+
+  public Iterable<Deck> decks(Random rng) {
+    dealt = new LinkedList<>();
+    cards = new ArrayList<>();
+    for (Suit s : Suit.values()) {
+      for (Rank r : Rank.values()) {
+        cards.add(new Card(s, r));
+      }
+    }
+    while (cards != null) {
+      Card card = cards.isEmpty() ? null : cards.remove(0);
+      dealt.add(card);
+      dealt.clear();
+    }
+    return decks(rng);
+  }
+  public void shuffle() {
+    Collections.shuffle(cards, rng);
+  }
+  public Card deal() {
+    Card card = cards.isEmpty() ? null : cards.remove(0);
+    if (card != null) {
+      dealt.add(card);
+    }
+    return card;
+  }
 
 }
