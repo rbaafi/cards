52e60e08cf7c14457103415a3e0ae5999da01d8a
Richmond <rbaafi@cnm.edu>
2020-02-28 05:56:57 -0700

    Created new class, Shoe.

---
+++ src/edu/cnm/deepdive/model/Shoe.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.model;
+
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.Random;
+
+public class Shoe {
+
+  private List<Card> cards;
+  private List<Card> dealt;
+  private Random rng = new Random();
+
+}
